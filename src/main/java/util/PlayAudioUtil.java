be7e157204fb3e53bd29eab87129191416cde164
dinesh kumar <chayaldinesh1@gmail.com>
2023-05-17 12:01:59 +0530

    single song & list of song is playing

--- src/main/java/util/PlayAudioUtil.java
+++ src/main/java/util/PlayAudioUtil.java
@@ -5,59 +5,58 @@ import bean.Song;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class PlayAudioUtil {
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     public void playSong(int id) {
-        DatabaseConnectionUtil databaseConnectionUtil=new DatabaseConnectionUtil();
-        Connection connection=databaseConnectionUtil.connectToDatabase();
+        DatabaseConnectionUtil databaseConnectionUtil = new DatabaseConnectionUtil();
+        Connection connection = databaseConnectionUtil.connectToDatabase();
         try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select  url from song where songId= '" + id + "';");
 
-            String url=null;
+            String url = null;
             while (resultSet.next()) {
                 url = resultSet.getString("url");
             }
-                AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(url));
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioStream);
-                String response = "";
-                while (!response.equals("Q")) {
-                    System.out.println("p=Play,s=Stop,r=Reset,q=Quit,N=next");
-                    response = scanner.next();
-                    response = response.toUpperCase();
-                    switch (response) {
-                        case ("P"):
-                            clip.start();
-                            break;
-                        case ("S"):
-                            clip.stop();
-                            break;
-                        case ("R"):
-                            clip.setMicrosecondPosition(1);
-                            break;
-                        case ("Q"):
-                            clip.close();
-                            break;
-                        case ("N"):
-                            clip.stop();
-                            continue;
-                        default:
-                            System.out.println("Your Choice is Wrong Enter Correct choice");
-                    }
-                    if (response.equals("Q") || response.equals("q"))
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(url));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            String response = "";
+            while (!response.equals("Q")) {
+                System.out.println("p=Play,s=Stop,r=Reset,q=Quit,N=next");
+                response = scanner.next();
+                response = response.toUpperCase();
+                switch (response) {
+                    case ("P"):
+                        clip.start();
                         break;
+                    case ("S"):
+                        clip.stop();
+                        break;
+                    case ("R"):
+                        clip.setMicrosecondPosition(1);
+                        break;
+                    case ("Q"):
+                        clip.close();
+                        break;
+                    case ("N"):
+                        clip.stop();
+                        continue;
+                    default:
+                        System.out.println("Your Choice is Wrong Enter Correct choice");
                 }
+                if (response.equals("Q") || response.equals("q"))
+                    break;
+            }
 
         } catch (UnsupportedAudioFileException ex) {
             throw new RuntimeException(ex);
@@ -71,97 +70,58 @@ public class PlayAudioUtil {
     }
 
 
-//
-//    public void pause() {
-//        if (status.equals("paused")) {
-//            System.out.println("audio is already paused");
-//            return;
-//        }
-//        this.currentFrame = this.clip.getMicrosecondPosition();
-//        clip.stop();
-//        status = "paused";
-//    }
-//
-//
-//    public void resumeAudio() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-//        if (status.equals("play")) {
-//            System.out.println("Audio is already " + "being played");
-//            return;
-//        }
-//        clip.close();
-//        resetAudioStream();
-//        clip.setMicrosecondPosition(currentFrame);
-//        this.play();
-//    }
-//
-//
-//    public void restart() throws IOException, LineUnavailableException, UnsupportedAudioFileException {
-//        clip.stop();
-//        clip.close();
-//        resetAudioStream();
-//        currentFrame = 0L;
-//        clip.setMicrosecondPosition(0);
-//        this.play();
-//    }
-//
-//
-//    public void stop() {
-//        currentFrame = 0L;
-//        clip.stop();
-//        clip.close();
-//    }
-//
-//    public void resetAudioStream() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-//        audioInputStream = AudioSystem.getAudioInputStream(new File(url).getAbsoluteFile());
-//        clip.open(audioInputStream);
-//        clip.loop(Clip.LOOP_CONTINUOUSLY);
-//    }
-//
+    public void playSong(List<Song> songList) {
 
+        System.out.println(songList.size());
+        Iterator<Song> iterator = songList.iterator();
+        String url="";
+        try {
+            while (iterator.hasNext()) {
+                url = iterator.next().getUrl();
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream((new File(url)));
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioStream);
+                String response = "";
 
-
-//    public void PlaySong(List<Song> songList) {
-//        try {
-//            AudioInputStream audioStream = AudioSystem.getAudioInputStream(();
-//            Clip clip = AudioSystem.getClip();
-//            clip.open(audioStream);
-//            String response = "";
-//            while (!response.equals("Q")) {
-//                System.out.println("P=Play,S=Stop,R=Reset,Q=Quit");
-//                response = scanner.next();
-//                response = response.toUpperCase();
-//                switch (response) {
-//                    case ("P"):
-//                        clip.start();
-//                        break;
-//                    case ("S"):
-//                        clip.stop();
-//                        break;
-//                    case ("R"):
-//                        clip.setMicrosecondPosition(1);
-//                        break;
-//                    case ("Q"):
-//                        clip.close();
-//                        break;
-//                    default:
-//                        System.out.println("Your Choice is Wrong Enter Correct choice");
-//                }
-//                if (response.equals("Q")||response.equals("q"))
-//                    break;
-//            }
-//        } catch (UnsupportedAudioFileException e) {
-//            throw new RuntimeException(e);
-//        } catch (LineUnavailableException e) {
-//            throw new RuntimeException(e);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//}
-//
-//
-//
-//
-//
-//
+                while (!response.equals("Q")) {
+                    System.out.println("P=Play,S=Stop,R=Reset,,N=Next,Q=Quit,");
+                    response = scanner.next();
+                    response = response.toUpperCase();
+                    switch (response) {
+                        case ("P"):
+                            clip.start();
+                            break;
+                        case ("S"):
+                            clip.stop();
+                            break;
+                        case ("R"):
+                            clip.setMicrosecondPosition(1);
+                            break;
+                        case ("N"):
+                            clip.close();
+                            break;
+                        case ("Q"):
+                            clip.close();
+                            break;
+                        default:
+                            System.out.println("Your Choice is Wrong Enter Correct choice");
+                    }
+                    if (response.equals("Q") || response.equals("q") || response.equals("N") || response.equals("n"))
+                        break;
+                }
+                if (response.equals("Q") || response.equals("q"))
+                break;
+            }
+        } catch (UnsupportedAudioFileException e) {
+            throw new RuntimeException(e);
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
+
+
