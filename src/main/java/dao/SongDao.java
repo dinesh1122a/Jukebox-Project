f90bd97a51b86bcdb4cb8d4b5be8641ddf10ce3e
dinesh kumar <chayaldinesh1@gmail.com>
2023-05-18 07:28:28 +0530

    search by method modified

--- src/main/java/dao/SongDao.java
+++ src/main/java/dao/SongDao.java
@@ -1,9 +1,10 @@
 package dao;
 
 import bean.Song;
-import exception.ArtistNameNotFoundException;
-import exception.GenreNotFoundException;
-import exception.SongNotFoundException;
+
+import util.DatabaseConnectionUtil;
+import util.PlayAudioUtil;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,15 +16,17 @@ import static util.DatabaseConnectionUtil.connectToDatabase;
 
 
 public class SongDao {
-    static Connection connection;
+    PlayAudioUtil playAudioUtil=new PlayAudioUtil();
+    public static Connection connection = DatabaseConnectionUtil.connectToDatabase();
+
     public static List<Song> getSongList() throws SQLException {
 
-        connection=connectToDatabase();
-        List<Song>songList=new ArrayList<>();
-        Statement statement= connection.createStatement();
-        ResultSet resultSet=statement.executeQuery("select * from song");
-        while (resultSet.next()){
-            Song song=new Song();
+        connection = connectToDatabase();
+        List<Song> songList = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from song");
+        while (resultSet.next()) {
+            Song song = new Song();
             song.setId(resultSet.getInt(1));
             song.setName(resultSet.getString(2));
             song.setArtist(resultSet.getString(3));
@@ -31,60 +34,73 @@ public class SongDao {
             song.setAlbum(resultSet.getString(5));
             song.setDuration(resultSet.getString(6));
             song.setUrl(resultSet.getString(7));
-            song =new Song(resultSet.getInt(1),resultSet.getString(2),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5),resultSet.getString(6),resultSet.getString(7));
+            song = new Song(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7));
             songList.add(song);
         }
         return songList;
 
     }
-
-    public List<Song> searchBy(int press, String name) throws SQLException, SongNotFoundException, ArtistNameNotFoundException, GenreNotFoundException {
-
-        List<Song> searchList = new ArrayList<>();
-
-        String choice = "";
-        switch (press) {
-            case 1:
-                choice = "Song name";
-                break;
-            case 2:
-                choice = "ArtistName";
-                break;
-            case 3:
-                choice = "Genre";
-                break;
-            default:
-                System.out.println("invalid input");
-        }
-        String sql = "SELECT * FROM song WHERE " + choice + " LIKE '%" + name + "%';";
+    public List<Song> searchByName(String Song) throws Exception {
+        DatabaseConnectionUtil databaseConnectionUtil = new DatabaseConnectionUtil();
+        Connection connection = databaseConnectionUtil.connectToDatabase();
+        Song song;
+        List<Song> songList = new ArrayList<>();
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);
+        ResultSet resultSet = statement.executeQuery("select * from song where name = '" + Song + "';");
         while (resultSet.next()) {
-           Song song=new Song();
-           song.setId(resultSet.getInt(1));
-            song.setName(resultSet.getString(2));
-            song.setArtist(resultSet.getString(3));
-            song.setGenre(resultSet.getString(4));
-            song.setAlbum(resultSet.getString(5));
-            song.setDuration(resultSet.getString(6));
-            song.setUrl(resultSet.getString(7));
-            song =new Song(resultSet.getInt(1),resultSet.getString(2),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5),resultSet.getString(6),resultSet.getString(7));
-            searchList.add(song);
+            song = new Song(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7));
+            songList.add(song);
         }
-
-
-
-        if (searchList.size() == 0 && press == 1) {
-            throw new SongNotFoundException("Song not Found");
-        }
-        if (searchList.size() == 0 && press == 2) {
-            throw new ArtistNameNotFoundException("Artist not Found");
-        }
-        if (searchList.size() == 0 && press == 3) {
-            throw new GenreNotFoundException("Genre not Found");
-        }
-
-        return searchList;
+        playAudioUtil.playSong(songList);
+        return songList;
     }
 
+
+
+
+    public List<Song> searchByArtist(String Song) throws Exception {
+        DatabaseConnectionUtil databaseConnectionUtil = new DatabaseConnectionUtil();
+        Connection connection = databaseConnectionUtil.connectToDatabase();
+        Song song;
+        List<Song> songList = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from song where artist = '" + Song + "';");
+        while (resultSet.next()) {
+            song = new Song(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7));
+            songList.add(song);
+        }
+        playAudioUtil.playSong(songList);
+        return songList;
+    }
+
+
+
+    public List<Song> searchByGenre(String Song) throws Exception {
+        DatabaseConnectionUtil databaseConnectionUtil = new DatabaseConnectionUtil();
+        Connection connection = databaseConnectionUtil.connectToDatabase();
+        Song song;
+        List<Song> songList = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from song where genre = '" + Song + "';");
+        while (resultSet.next()) {
+             song = new Song(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7));
+            songList.add(song);
+        }
+        playAudioUtil.playSong(songList);
+        return songList;
+    }
+    public List<Song> searchByAlbum(String Song) throws Exception {
+        DatabaseConnectionUtil databaseConnectionUtil = new DatabaseConnectionUtil();
+        Connection connection = databaseConnectionUtil.connectToDatabase();
+        Song song;
+        List<Song> songList = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from song where album = '" + Song + "';");
+        while (resultSet.next()) {
+            song = new Song(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7));
+            songList.add(song);
+        }
+        playAudioUtil.playSong(songList);
+        return songList;
+    }
 }
