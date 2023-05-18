4a81a01065fcc73e79fd1eb8ed885d4bd1a15962
dinesh kumar <chayaldinesh1@gmail.com>
2023-05-18 23:11:51 +0530

    method already right

--- src/main/java/dao/PlaylistDao.java
+++ src/main/java/dao/PlaylistDao.java
@@ -32,19 +32,19 @@ public class PlaylistDao {
 
     }
 
-        public boolean createPlayList() throws SQLException {
+        public void createPlayList() throws SQLException {
             DatabaseConnectionUtil databaseConnectionUtil = new DatabaseConnectionUtil();
             Connection connection = databaseConnectionUtil.connectToDatabase();
             System.out.println("Enter Id");
             Scanner scanner = new Scanner(System.in);
-            int Id = scanner.nextInt();
+            int playlistid = scanner.nextInt();
             System.out.println("Enter Song Name");
-            String Name = scanner.next();
+            String name = scanner.next();
             System.out.println("Enter Date like YYYY-MM-DD");
-            String dateofCreation = scanner.next();
+            String dateofcreation = scanner.next();
             Statement statement = connection.createStatement();
-            statement.executeUpdate("insert into  Playlist1 values (" + Id + ",'" + Name + "','" + dateofCreation + "');");
-            return false;
+            statement.executeUpdate("insert into  playlist values (" + playlistid + ",'" + name + "','" + dateofcreation + "');");
+
         }
 
     }
