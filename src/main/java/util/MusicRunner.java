281e267f51dd96a9d3f0bc230112d8991e6e9073
dinesh kumar <chayaldinesh1@gmail.com>
2023-05-18 23:24:16 +0530

    call main method

--- src/main/java/util/MusicRunner.java
+++ src/main/java/util/MusicRunner.java
@@ -1,13 +1,5 @@
 package util;
-
-
-import dao.PlaylistDao;
-
-import java.util.Scanner;
-
 public class MusicRunner {
-    Scanner scanner = new Scanner(System.in);
-
 
     public static void main(String[] args) throws Exception {
         DisplayUtil displayUtil = new DisplayUtil();
