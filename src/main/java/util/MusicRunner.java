3f6dba1553c711a640de24f3e857946d192b7209
dinesh kumar <chayaldinesh1@gmail.com>
2023-05-18 13:49:00 +0530

    call the mainmenu method in main method

--- src/main/java/util/MusicRunner.java
+++ src/main/java/util/MusicRunner.java
@@ -1,16 +1,6 @@
 package util;
 
-import bean.Song;
-import dao.SongDao;
 
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.awt.*;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class MusicRunner {
