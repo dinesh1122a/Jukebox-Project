6c562e030e4ef34f90d4fcb1f1a770546e450633
dinesh kumar <chayaldinesh1@gmail.com>
2023-05-19 07:42:16 +0530

    write test cases & dependency added

--- src/test/java/SongTest.java
+++ src/test/java/SongTest.java
@@ -1,5 +1,50 @@
+import bean.Song;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 
 public class SongTest {
+    Song song =new Song();
+    @Before
+    public void setUp()
+    {
+        song = new Song(4," wo Noor","ap dhillon","mix","heart","00:02:34","D:\\java\\juke\\src\\main\\resources\\Wo-Noor---Ap-Dhillon(PagalWorld).wav");
+    }
+    @After
+    public void tearDown()
+
+    {
+        song=null;
+    }
+    @Test
+    public void checkSong()
+    {
+        assertEquals(4,song.getId());
+    }
+    @Test
+    public void checkSongbyName()
+    {
+        assertEquals(" wo Noor",song.getName());
+    }
+    @Test
+    public void checkgenre()
+    {
+        assertEquals("mix",song.getGenre());
+    }
+    @Test
+    public void checkalbumName()
+    {
+        assertEquals("heart",song.getAlbum());
+    }
+    @Test
+    public void checkartistName()
+    {
+        assertEquals("ap dhillon",song.getArtist());
+    }
+
 
 }
+
+
